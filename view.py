f24f6ef8d4898e17be75d4713be1b3c932a1aca3
Bright Soul <andrejpopov8393@gmail.com>
2025-05-13 09:19:04 +0700

    Update view.py

--- view.py
+++ view.py
@@ -6,20 +6,20 @@ class View:
     def __init__(self, page):
         self.page = page
 
-        # Слайдер для выбора длины пароля
+       
         self.slider = Slider(min=4, max=15, value=8, divisions=11, label="{value}")
-        # Текст для отображения длины
+      
         self.len_text = Text(value=f"Длина пароля: {int(self.slider.value)}", size=16)
-        # Кнопка для генерации пароля
+   
         self.btn_gen = ElevatedButton(text="Сгенерировать")
-        # Кнопка для добавления пароля в список
+       
         self.btn_add = ElevatedButton(text="Добавить в список")
-        # Текст для вывода результата
+        
         self.result = Text(value="", size=20)
-        # Список для хранения паролей
+       
         self.pass_list = ListView()
 
-        # Компоновка элементов
+      
         self.page.title = "Генератор паролей"
         self.page.add(
             Column(
