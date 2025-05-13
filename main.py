50c98c053163a5cf9e0bc79722f35d66c1673d09
Bright Soul <andrejpopov8393@gmail.com>
2025-05-13 09:13:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from flet import Page
 from model import Model
 from view import View
 from controller import Controller
-
 def main(page: Page):
     model = Model()  
-    view = View(page) 
+    view = View(page)  
     Controller(model, view)  
-
-flet.app(target=main)  
+    
+flet.app(target=main)
