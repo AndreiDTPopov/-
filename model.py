a097dc16a9ac9d7b65f173cb17c2ae2d70264edf
Bright Soul <andrejpopov8393@gmail.com>
2025-05-13 09:18:19 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 # model.py
 import random
 import string
+
 class Model:
     def __init__(self):
-        self.passwords = []  # Список для хранения паролей
+        self.passwords = []  
+
     def gen_pass(self, length):
-        # Генерация пароля из букв, цифр и символов
-        chars = string.ascii_letters + string.digits + string.punctuation
+        
+        chars = string.ascii_letters + string.digits + string.punctuation + " " 
         return ''.join(random.choice(chars) for _ in range(length))
+
     def add_pass(self, password):
-        # Добавление пароля в список
+        
         self.passwords.append(password)
